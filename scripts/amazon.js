16901375c73cf4e8240b7af32318cb6933cc0e64
akhirai560 <akhileshgzp500@gmail.com>
2025-05-23 17:43:05 +0530

    added carts arr

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -55,3 +55,24 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach(button => {
+  button.addEventListener('click', (event) => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+
+    cart.forEach(item => {
+      if(item.productId === productId) {
+        matchingItem = item;
+      }
+    });
+
+    if(matchingItem) {
+      matchingItem.quantity += 1
+    } else {
+      cart.push({
+        productId, quantity: 1
+      });
+    }
+  });
+})
